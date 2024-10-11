47df35b1932c3c5e316416add9489fefdb2454ff
kotharihimanshi <himanshikothari9@gmail.com>
2024-10-11 19:16:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+function locomotiveAnimation() {               //to make locomotive and scrollTrigger work together
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true,
+
+        // for tablet smooth
+        tablet: { smooth: true },
+
+        // for mobile
+        smartphone: { smooth: true }
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length
+                ? locoScroll.scrollTo(value, 0, 0)
+                : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return {
+                top: 0,
+                left: 0,
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
+        }
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+}
+
+
+function page2Animation() {
+    var page2Center = document.querySelector(".page2-center");
+    var video = document.querySelector(".page-2 video");
+
+    page2Center.addEventListener("click", function () {
+        video.play()
+        gsap.to(video, {
+            transform: "scaleX(1) scaleY(1)",
+            opacity: 1,
+            borderRadius: 0
+        })
+    })
+
+    video.addEventListener("click", function () {
+        video.pause()
+        gsap.to(video, {
+            transform: "scaleX(0.7) scaleY(0)",
+            opacity: 0,
+            borderRadius: "30px"
+        })
+    })
+}
+
+function navigatetoPage() {
+    const url = "location.html"; 
+    window.open(url, '_blank');
+}
+
+ function gotoHome(){
+    window.location.href = "index.html";
+}
+locomotiveAnimation()
+page2Animation()
---
+++ script1.js
@@ -0,0 +1,3 @@
+function gotoHome(){
+    window.location.href = "index.html";
+}
